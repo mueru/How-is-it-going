dafea63d6eca0cde9b2c576f0f7cc98195ebda35
mueru <mueru15@gmail.com>
2020-01-10 20:47:19 +0900

    init

---
+++ How_is_it_going/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for How_is_it_going project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'How_is_it_going.settings')
+
+application = get_asgi_application()
